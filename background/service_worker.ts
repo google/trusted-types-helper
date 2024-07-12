40571200a8649900aa4accd8c93d1988e944bed6
Mayra Robles <77298410+Mayra-Robles@users.noreply.github.com>
2024-07-12 13:04:31 +0000

    Updated to use dynamic rules to inject TT header
    Bug:347986675
    
    Change-Id: Iefd50232be8a14df4388d7a22bf5134b8752a3c8

--- background/service_worker.ts
+++ background/service_worker.ts
@@ -1,11 +1,77 @@
 /// <reference types="chrome"/>
-var violations : string[] = [];
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+import {DefaultPolicyData, Message, Violation, Violations} from "../common/common.js";
+
+const CSP_HEADER = 'content-security-policy';
+const CSP_HEADER_REPORT_ONLY = 'Content-Security-Policy-Report-Only';
+const TRUSTED_TYPES_DIRECTIVE =  "require-trusted-types-for 'script'";
+
+var violations : Violations= new Violations();
+var defaultPolicyData: DefaultPolicyData = {};
+
+
+// Listens to the content script
+chrome.runtime.onMessage.addListener((msg: Message, sender, sendResponse) => {
   console.log(msg);
   console.log(sender.tab?.url);
-  if (msg.payload) {
-    violations.push(msg.payload);
-  } else if (msg.command == "listViolations") {
-    sendResponse({violations})
+
+  switch (msg.type) {
+    case 'violation':
+      if (msg.violation)  {
+        violations[msg.violation.getType()].push(msg.violation);
+      }
+      break;
+    case 'listViolations':
+      sendResponse({ violations });
+      break;
+    case 'defaultPolicySet':
+      defaultPolicyData.wasSet = msg.defaultPolicySet;
+      break;
+    case 'defaultPolicyCreationFailed':
+      defaultPolicyData.creationFailed = msg.defaultPolicyCreationFailed;
+      break;
+    case 'getDefaultPolicyData':
+      sendResponse(defaultPolicyData);
+      break;
   }
 });
+
+if (chrome.webRequest !== undefined) {
+  chrome.webRequest.onHeadersReceived.addListener(
+    checkHeaderForTrustedTypes,
+      ({urls: ['<all_urls>']}), ['responseHeaders']);
+}
+
+function checkHeaderForTrustedTypes(details: chrome.webRequest.WebResponseHeadersDetails): void {
+  // Check if there is already a csp in a header with the trusted types directive
+  if (details && details.responseHeaders) {
+    if (!(details.responseHeaders.some( (header: chrome.webRequest.HttpHeader) => header.name == CSP_HEADER &&
+      header.value && header.value.includes(TRUSTED_TYPES_DIRECTIVE)) )) {
+      console.log("Changed the headers!");
+      chrome.declarativeNetRequest.updateDynamicRules({
+        addRules: [
+          {
+            id: 1,
+            priority: 1,
+            action: {
+              type: chrome.declarativeNetRequest.RuleActionType.MODIFY_HEADERS,
+              responseHeaders: [
+                {
+                  header: CSP_HEADER_REPORT_ONLY,
+                  operation: chrome.declarativeNetRequest.HeaderOperation.APPEND,
+                  value: TRUSTED_TYPES_DIRECTIVE + ";",
+                },
+              ],
+            },
+            condition: {
+              "resourceTypes": [
+                "main_frame" as any
+              ]
+            },
+          },
+        ],
+        removeRuleIds: [1],
+      });
+    }
+  }
+}
+
--- src/content.js
+++ src/content.js
@@ -1,12 +1,48 @@
 /**
  * @fileoverview Description of this file.
  */
-// Register default policy
-self.trustedTypes.createPolicy('default', {
-  createHTML: (string => {
-    //TODO implement default policy processing/sanitizing
-    console.log(string);
-    window.postMessage({ "payload": string }, '*');
-    return string;
-  })
+
+// Alert when there is an error in case the user already has a default policy,
+// the extension policy may be set first and when the user's default policy
+// is set an error occurs.
+addEventListener("error", (event) => {
+  if (error.name == 'TypeError' && error.message.includes('Policy with name "default" already exists')) {
+    alert("Failure when Trusted Types Helper extension tried to set a default policy.", error);
+  }
 });
+
+try {
+  self.trustedTypes.createPolicy('default', {
+    createHTML: (string => {
+      //TODO implement default policy processing/sanitizing
+      console.log(string);
+
+      const htmlViolation = { type: 'HTML',
+                               data : string,
+                               timestamp: Date.now()
+                              };
+      const msg = {
+        type: 'violation',
+        violation: htmlViolation
+      };
+
+      window.postMessage(msg, '*');
+      return string;
+    })
+  });
+} catch(error) {
+  console.error('Trusted Types Default Policy Creation Failed:', error.message);
+  const msg = {
+    type: 'defaultPolicyCreationFailed',
+    defaultPolicyCreationFailed: Date.now()
+  }
+  window.postMessage(msg, '*');
+}
+
+
+// When page reloads, these lines will get executed
+const msg = {
+  type: 'defaultPolicySet',
+  defaultPolicySet: Date.now()
+}
+window.postMessage(msg, '*');
