3d77b91c04dc82aa200e9d335fed1a1c0107639c
Mayra Robles <mayrarobles@google.com>
2024-07-12 13:04:31 +0000

    Updated to use dynamic rules to inject TT header
    Bug:347986675
    
    Change-Id: Iefd50232be8a14df4388d7a22bf5134b8752a3c8

---
+++ common/common.ts
@@ -0,0 +1,45 @@
+export interface DefaultPolicyData {
+    wasSet?: Date;
+    creationFailed?: Date;
+};
+
+export type ViolationType = 'HTML' | 'Script' | 'URL';
+
+export class Violation {
+    private data: string;
+    private type: ViolationType;
+    private timestamp: Date;
+
+    public constructor(data: string, type: ViolationType, timestamp: Date) {
+        this.data = data;
+        this.type = type;
+        this.timestamp = timestamp;
+    }
+
+    public getData() { return this.data; }
+
+    public getType() { return this.type; }
+
+    public getTimestamp() { return this.timestamp; }
+}
+
+type ViolationDataType = {
+    [key in ViolationType] : Array<Violation>;
+};
+
+export class Violations implements ViolationDataType {
+    public HTML: Array<Violation> = [];
+    public Script: Array<Violation> = [];
+    public URL: Array<Violation> = [];
+
+    constructor () {}
+}
+
+export interface Message {
+    type: 'violation' | 'listViolations' | 'defaultPolicySet' | 'defaultPolicyCreationFailed' | 
+          'getDefaultPolicyData';
+    violation?: Violation;
+    defaultPolicySet?: Date;
+    defaultPolicyCreationFailed?: Date;
+  }
+
--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -2,24 +2,42 @@
 // import {chrome} from '@types/chrome';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DefaultPolicyData } from "../../../common/common";
+import { NgClass } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'trusted-types-helper-ui';
+  message = 'No message yet.';
+  isSuccess = false;
+
 
   ngOnInit() {
     console.log('OnInit');
     (async () => {
-      const response = await chrome.runtime.sendMessage({command: "listViolations"});
+
+      const response = await chrome.runtime.sendMessage({type: "listViolations"});
       // do something with response here, not outside the function
-      console.log(response);
+    })();
+
+    // Send message asking for the last time the default policy was overwritten
+    (async () => {
+      const defaultPolicyData : DefaultPolicyData =
+                        await chrome.runtime.sendMessage({type: "getDefaultPolicyData"});
+      if (defaultPolicyData.creationFailed) {
+        this.isSuccess = false;
+        this.message = 'Default policy creation failed.';
+      } else if (defaultPolicyData.wasSet) {
+        this.isSuccess = true;
+        this.message = 'Default policy was created.' ;
+      }
     })();
   }
 }
