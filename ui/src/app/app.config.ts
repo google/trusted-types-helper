574d71f7e27b5e2f52f48ff742a549986c48da64
Aaron Shim <5382864+aaronshim@users.noreply.github.com>
2025-02-05 20:02:55 +0000

    Remove Angular routing to remove exceptions
    Change-Id: I35b91e044e86519cbb959c28c208489b033fab64

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -15,15 +15,12 @@
  */
 
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHighlightOptions } from 'ngx-highlightjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
     provideAnimationsAsync(),
     provideHighlightOptions({
       coreLibraryLoader: () => import('highlight.js/lib/core'),
